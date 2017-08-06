d2cb8165fac4dfa94be4fcd08362002b0b91a30d
Sujanth <sujanth1987@gmail.com>
2017-08-06 21:15:22 +1000

    CollectionView cell created and connected with custom class

---
+++ NamesToFaces/PersonCell.swift
@@ -0,0 +1,16 @@
+//
+//  PersonCell.swift
+//  NamesToFaces
+//
+//  Created by Sujanth Sebamalaithasan on 6/8/17.
+//  Copyright © 2017 Sujanth Sebamalaithasan. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    
+}
